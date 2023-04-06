7ae3c8bab39c61e1f3d434306a985870a634a80e
Gonzalo Olave <growolff@gmail.com>
2023-04-06 17:39:17 -0400

    add arguments for controling lapse and quantity of photos

--- timelapse.py
+++ timelapse.py
@@ -1,15 +1,32 @@
 from picamera import PiCamera
 from os import system
 from time import sleep
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Este programa permite tomar timelapses con una rPI Cam')
+parser.add_argument('-tt','--totaltime', help='Tiempo total del timelapse en segundos', required=False, type=int)
+parser.add_argument('-l','--lapse',help='Tiempo entre fotos en segndos', required=True, type=int)
+parser.add_argument('-c', '--cantidad', help='Cantidad de imagenes', required=True, type=int)
+
+args = parser.parse_args()
 
 camera = PiCamera()
 camera.resolution = (1024,768)
+camera.vflip = True
+camera.hflip = True
 
-for i in range(240):
+totaltime = args.totaltime
+lapse = args.lapse
+cantidad = args.cantidad
+
+   
+for i in range(cantidad):
     name = 'image{0:04d}.jpg'.format(i)
     camera.capture(name)
     print('photo %s taken'%(name))
-    sleep(600)
+    sleep(lapse)
 
 print('done')
+
 
